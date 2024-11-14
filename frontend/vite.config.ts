44f5d07b3b853b1429f86732895de12c2fcbead5
Mikhail Nazaretyan (pluggedinlife) <sgas5570@gmail.com>
2024-11-14 15:22:32 +0100

    Minor code enhancements: add and initialize tailwindcss and add socket.io

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000", // Your backend server URL
+        changeOrigin: true, // This ensures that the origin of the request is changed to the target URL
+        secure: false, // Set to false if you're using HTTP instead of HTTPS
+        ws: true, // If you're using WebSockets (for example with Socket.IO)
+      },
+    },
+  },
+});
