c8088880f00f5ee9581892f67f1a872baec944ee
Mikhail Nazaretyan (pluggedinlife) <sgas5570@gmail.com>
2024-11-24 18:16:32 +0200

    minor code enhancements

---
+++ frontend/src/components/VMessage.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const props = defineProps({
+  message: {
+    type: String,
+  },
+});
+</script>
+
+<template>
+  <div class="p-2 flex justify-end">
+    <span
+      class="bg-blue-200 dark:text-gray-800 px-2 py-1 rounded-xl max-w-full break-words"
+      >{{ props.message }}</span
+    >
+  </div>
+</template>
--- frontend/src/views/VChatView.vue
+++ frontend/src/views/VChatView.vue
@@ -1,14 +1,19 @@
 <template>
-  <div class="bg-blue-200 flex flex-col">
-    <div class="h-screen relative overflow-auto pb-10">
-      <ul>
-        <li v-for="(item, index) in messageList" :key="index">{{ item }}</li>
-      </ul>
+  <div class="flex flex-col">
+    <div class="h-screen relative overflow-auto pb-10 flex flex-col">
+      <VMessage
+        v-for="(item, index) in messageList"
+        :key="index"
+        :message="item"
+      />
     </div>
 
-    <div class="bg-red-200 absolute bottom-0 w-full flex flex-col">
-      <label for="messageInput"> put the message in</label>
+    <div
+      class="absolute bottom-0 w-full flex flex-col px-2 pb-1 border-t border-t-gray-500"
+    >
       <input
+        placeholder="Message"
+        class="dark:bg-gray-700 focus:outline-none p-1"
         type="text"
         id="messageInput"
         v-model="message"
@@ -21,9 +26,10 @@
 
 <script setup lang="ts">
 import { ref } from 'vue';
+import VMessage from '../components/VMessage.vue';
 
-let message = ref('');
-let messageList = ref(['']);
+let message = ref('' as string);
+let messageList = ref([] as string[]);
 
 function handleSubmit() {
   messageList.value.push(message.value);
